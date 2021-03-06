0c5d12fa564aaab2cbede2f587ae84e5375ba74e
EaswarUS <80040514+EaswarUS@users.noreply.github.com>
2021-03-06 22:11:35 +0530

    Add files via upload

---
+++ WeekDay.java
@@ -0,0 +1,33 @@
+package assesment_1;
+
+public class WeekDay {
+	public String monday() {
+		String Day1 = "weekday";
+		System.out.println(Day1);
+		return Day1;
+	}
+
+	public String tuesday() {
+		String Day2 = "weekday";
+		System.out.println(Day2);
+		return Day2;
+	}
+
+	public String wednessday() {
+		String Day3 = "weekday";
+		System.out.println(Day3);
+		return Day3;
+	}
+
+	public String sunday() {
+		String Day4 = "weekday";
+		System.out.println(Day4);
+		return Day4;
+	}
+
+	public static void main(String[] args) {
+		 WeekDay obj = new  WeekDay();
+		obj.wednessday();
+	}
+
+}
